16e7405414ca1693c765ebb45ea81a397f29fe14
Patel <alpna.patel@capgemini.com>
2022-07-05 10:30:11 +0530

    Final Code

--- src/App.js
+++ src/App.js
@@ -2,20 +2,15 @@
 import { connect } from 'react-redux';
 import React, {useContext} from 'react';
 import './App.css';
-import AddTodo from "./MyComponents/AddTodo";
 import Footer from './MyComponents/Footer';
 import Header from "./MyComponents/Header";
 import Todos from "./MyComponents/Todos";
-import {todo} from './index'
+
 
 function App() {
-
-  const user = useContext(todo);
-  console.log(user)
   return (
     <div className="App">
       <Header title="Assignment-one [Task Management]" searchBar={false} />
-      <AddTodo />
       <Todos />
       <Footer/>
     </div>
--- src/MyComponents/Todos.jsx
+++ src/MyComponents/Todos.jsx
@@ -1,86 +1,242 @@
-import React, { useEffect, useState,useContext } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import TodoItem from "./TodoItem";
-import {todo} from '../../src/index'
+import { todo } from '../../src/index'
+import { v4 as uuidv4 } from 'uuid';
 
+const regexPattern = new RegExp(/^([A-Za-z]|[0-9]|_)+$/)
+const innerPageStyle = {
+  marginTop: 30,
+};
+
+export function todosAcation(state = [], action) {
+
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [...state.map((item) => {
+        if (item.id === 1) {
+          item.cards.push(action.value)
+        }
+        return item
+      })]
+    case 'REMOVE_TODO':
+      return [...state.map((item) => {
+        if (item.id === action.id) {
+          var filtered = item.cards.filter(function (value) {
+            return value.cid !== action.cid;
+          });
+          item.cards = [...filtered]
+          return item
+        } else {
+          return item
+        }
+      })]
+    case 'MOVE_LEFT_TODO':
+      
+      return [...state.map((item) => {
+        if (item.id === action.id && action.id > 1) {
+            var filtered = item.cards.filter(function (value) {
+                return value.cid !== action.item.cid;
+            });
+            item.cards = [...filtered]
+            return item
+        } else if (item.id === action.id - 1) {
+            item.cards.push(action.item)
+        }
+        return item
+    })]
+    case 'MOVE_RIGHT_TODO':
+      return [...state.map((item) => {
+        if (item.id === action.id && action.id < 4) {
+          var filtered = item.cards.filter(function (value) {
+            return value.cid !== action.item.cid;
+          });
+          item.cards = [...filtered]
+          return item
+        } else if (item.id === action.id + 1) {
+          item.cards.push(action.item)
+        }
+        return item
+      })]
+    default:
+      return state
+  }
+}
 
 function Todos(props) {
   let myStyle = {
     minHeight: "70vh",
     margin: "10px auto",
   };
-  const user = useContext(todo);
+
+  const todoSContext = useContext(todo);
+  const [todosListContext, settodosListContext] = useState([])
   const [backlogList, setBacklogList] = useState([])
   const [todosList, settodosList] = useState([])
   const [inProgressList, setInProgress] = useState([])
   const [doneList, setdoneList] = useState([])
-  
+  const [title, setTitle] = useState("");
+  const submit = (e) => {
 
-  const { todos } = props
+    e.preventDefault();
+    if (!title) {
+      alert("Backlog can't be blank");
+    }
+    else if (!regexPattern.test(title)) {
+      alert("Not Matched")
+      setTitle("");
+
+    }
+    else {
+
+      setBacklogList([...backlogList, { name: title, stage: 0, cid: uuidv4() }]);
+      setTitle("");
+    }
+  }
+
+
 
   const listItem = (todos, id) => {
     let data = []
-    user?.forEach(element => {
+    todos?.forEach(element => {
       if (element.id === id) {
         data = [...element.cards]
       }
     });
     return data
   }
+
+
+
   useEffect(() => {
-    setBacklogList(listItem(todos, 1))
-    settodosList(listItem(todos, 2))
-    setInProgress(listItem(todos, 3))
-    setdoneList(listItem(todos, 4))
-  }, [user])
+    setBacklogList(listItem(todoSContext, 1))
+    settodosList(listItem(todoSContext, 2))
+    setInProgress(listItem(todoSContext, 3))
+    setdoneList(listItem(todoSContext, 4))
+  }, [todosListContext])
+
+  useEffect(() => {
+    settodosListContext(todoSContext)
+  }, [todoSContext])
 
   return (
-    <div className="container my-3" style={myStyle}>
-      <h3 className="my-3">Backlog List</h3>
-      <div class="row">
-        <div class="col-sm-3">
-          <h3>Backlog</h3>
-          {backlogList.map((item) => {
-            return <TodoItem {...item} removeTodoAction={()=>props.removeTodoAction(item.cid,1)} moveLeftTodoAction={()=>props.moveLeftTodoAction(item,1)} moveRightAction={()=>props.moveRightAction(item,1)}/>
-          })}
-        </div>
-        <div class="col-sm-3">
-          <h3>Todo</h3>
-          {todosList.map((item) => {
-            return <TodoItem {...item} removeTodoAction={()=>props.removeTodoAction(item.cid,2)} moveLeftTodoAction={()=>props.moveLeftTodoAction(item,2)} moveRightAction={()=>props.moveRightAction(item,2)}/>
-          })}
-        </div>
-        <div class="col-sm-3">
-          <h3>In-Progress</h3>
-          {inProgressList.map((item) => {
-            return <TodoItem {...item} removeTodoAction={()=>props.removeTodoAction(item.cid,3)} moveLeftTodoAction={()=>props.moveLeftTodoAction(item,3)} moveRightAction={()=>props.moveRightAction(item,3)}/>
-          })}
-        </div>
-        <div class="col-sm-3">
-          <h3>Done</h3>
-          {doneList.map((item) => {
-            return <TodoItem {...item} removeTodoAction={()=>props.removeTodoAction(item.cid,4)} moveLeftTodoAction={()=>props.moveLeftTodoAction(item,4)} moveRightAction={()=>props.moveRightAction(item,4)}/>
-          })}
+    <>
+      <div className="container my-9 ">
+        <h4 style={innerPageStyle}>Add New Backlog </h4>
+        <form onSubmit={submit}>
+          <div className="mb-3">
+            <input
+              type="text"
+              placeholder='Please Add Backlog…'
+              value={title}
+              onChange={(e) =>
+                setTitle(e.target.value)
+              }
+              className="form-control"
+              id="title"
+              aria-describedby="emailHelp"
+            />
+          </div>
+          <button type="submit" className="btn btn-sm btn-primary">
+            Add Backlog
+          </button>
+        </form>
+      </div>
+      <div className="container my-3" style={myStyle}>
+        <h3 className="my-3">Backlog List</h3>
+        <div className="row">
+          <div className="col-sm-3">
+            <h3>Backlog</h3>
+            {backlogList.map((item) => {
+              return <TodoItem {...item}  key={item.cid} removeTodoAction={() => settodosListContext(todosAcation(todosListContext, {
+                cid: item.cid,
+                id: 1,
+                type: 'REMOVE_TODO'
+              }))} moveLeftTodoAction={
+                () => settodosListContext(todosAcation(todosListContext, {
+                  item,
+                  id: 1,
+                  type: 'MOVE_LEFT_TODO'
+                }))}
+
+                moveRightAction={
+                  () => settodosListContext(todosAcation(todosListContext, {
+                    item,
+                    id: 1,
+                    type: 'MOVE_RIGHT_TODO'
+                  }))}
+              />
+            })}
+          </div>
+          <div className="col-sm-3">
+            <h3>Todo</h3>
+            {todosList.map((item) => {
+              return <TodoItem {...item}  key={item.cid} removeTodoAction={() => settodosListContext(todosAcation(todosListContext, {
+                cid: item.cid,
+                id: 2,
+                type: 'REMOVE_TODO'
+              }))} moveLeftTodoAction={
+                () => settodosListContext(todosAcation(todosListContext, {
+                  item,
+                  id: 2,
+                  type: 'MOVE_LEFT_TODO'
+                }))}
+
+                moveRightAction={
+                  () => settodosListContext(todosAcation(todosListContext, {
+                    item,
+                    id: 2,
+                    type: 'MOVE_RIGHT_TODO'
+                  }))} />
+            })}
+          </div>
+          <div className="col-sm-3">
+            <h3>In-Progress</h3>
+            {inProgressList.map((item) => {
+              return <TodoItem {...item}  key={item.cid} removeTodoAction={() => settodosListContext(todosAcation(todosListContext, {
+                cid: item.cid,
+                id: 3,
+                type: 'REMOVE_TODO'
+              }))} moveLeftTodoAction={
+                () => settodosListContext(todosAcation(todosListContext, {
+                  item,
+                  id: 3,
+                  type: 'MOVE_LEFT_TODO'
+                }))}
+
+                moveRightAction={
+                  () => settodosListContext(todosAcation(todosListContext, {
+                    item,
+                    id: 3,
+                    type: 'MOVE_RIGHT_TODO'
+                  }))} />
+            })}
+          </div>
+          <div className="col-sm-3">
+            <h3>Done</h3>
+            {doneList.map((item) => {
+              return <TodoItem {...item} key={item.cid} removeTodoAction={() => settodosListContext(todosAcation(todosListContext, {
+                cid: item.cid,
+                id: 4,
+                type: 'REMOVE_TODO'
+              }))} moveLeftTodoAction={
+                () => settodosListContext(todosAcation(todosListContext, {
+                  item,
+                  id: 4,
+                  type: 'MOVE_LEFT_TODO'
+                }))}
+
+                moveRightAction={
+                  () => settodosListContext(todosAcation(todosListContext, {
+                    item,
+                    id: 4,
+                    type: 'MOVE_RIGHT_TODO'
+                  }))} />
+            })}
+          </div>
         </div>
       </div>
-    </div>
+    </>
+
   );
 }
-const mapDispatchToProps = (dispatch) => {
-  return {
-    // dispatching plain actions
-    moveLeftTodoAction: (cid,id) => dispatch({ type: 'MOVE_LEFT_TODO',value:{
-      cid,
-      id
-    } }),
-    moveRightAction: (cid,id) => dispatch({ type: 'MOVE_RIGHT_TODO',value:{
-      cid,
-      id
-    } }),
-    removeTodoAction: (cid,id) => dispatch({ type: 'REMOVE_TODO',value:{
-      cid,
-      id
-    } }),
-  }
-}
-
 export default Todos;
